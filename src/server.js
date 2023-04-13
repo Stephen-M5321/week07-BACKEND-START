fef820fb3467586ed5be180ddf448d9e63eb3aca
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-04-13 16:27:31 +0100

    delete book added

--- src/server.js
+++ src/server.js
@@ -5,79 +5,60 @@ const Book = require("./modbooks/model")
 
 const app = express()
 
-
-app.use("/", express.static("AnotherRoute"))
-app.use("/books", express.static("books"))
-// app.use("/music", express.static("music"))
-// app.use("/movies", express.static("movies"))
-// app.use("/football", express.static("football"))
-
-
 app.use (express.json())
 
-app.get("/book", (request, response) => {
-    const book = {
-        title:"Lord of the rings",
-        author:"Tolkein",
-        genre:"fantasy"
-    }
-
-    const successResponse = {
-        message: "Response sent successfully",
-        book: book
-    }
-
-    // response.send("hello from the book route")
-
-    response.send(successResponse)
-} )
-
-app.get("/anotherRoute", (request, response) => {
-    response.send("hello from the another route")
-} )
-
-app.get("/books/getallbooks", async (request, response) => {
-    const bookList = await Book.find ({ 
-        
-    })
-    console.log(bookList)
-
-    const successSendResponse = {
-        message: "Retrieved list of books successfully",
-        book: bookList
-    }
-    // response.send(successSendResponse)
-    response.status(200).json(successSendResponse)
+app.get("/books/getallbooks", async (req, res) => {
+    try {
+        const books = await Book.find({});
+        // console.log("books: ", typeof books, books);      (used for testing)
+        const successResponse = {
+            message: "success",
+            books: books,
+        };
+    
+        res.status(200).json(successResponse)   
+      } catch (error) {
+        console.log(error);  
+        }
 })
 
-app.post("/books/addbook", async (request, response) => {
-    const newBook = await Book.create ({ 
-        title: request.body.title,
-        author: request.body.author,
-        genre: request.body.genre
+app.delete("/books/deletebook", async (req, res) => {
+    const deletedItem = await Book.deleteOne ({ 
+        // title: request.body.title,
+        // author: request.body.author,
+        title: req.body.title,
+
     })
-    console.log(newBook)
-
-    // console.log(request.body)
-
-    // const newBook = {
-    //     id: "12345",
-    //     title: request.body.title,
-    //     author: request.body.author,
-    //     genre: request.body.genre,
-    // }
+    console.log(deletedItem)
 
     const successSendResponse = {
-        message: "Response sent successfully",
-        book: newBook
+        message: "Deleted successfully",
+        book: deletedItem
     }
-
-    // response.send("hello from the book route")
-
     // response.send(successSendResponse)
-    response.status(201).json(successSendResponse)
+    res.status(200).json(successSendResponse)
+})
 
-    // response.send("Hello from the post route")
+app.post("/books/addbook", async (req, res) => {
+    try{       
+        const newBook =  await Book.create({
+          author: req.body.author,
+          genre: req.body.genre,
+          title: req.body.title,
+        });
+  
+        // console.log(typeof newBook);                   (used for testing)
+        // console.log(newBook);                          (used for testing)
+  
+        const successResponse = {
+          messsage: "success",
+          newBook: newBook,
+        };
+  
+        res.status(201).json(successResponse)
+      } catch(error) {
+        console.log(error);
+      }
 } )
 
 app.listen(5001, () => console.log("server is listening on 5001"))
