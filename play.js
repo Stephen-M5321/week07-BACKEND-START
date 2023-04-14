5dd181a33fa7c72e74d7e0e675f103ba35f4723e
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-04-14 17:35:33 +0100

    refactored

---
+++ play.js
@@ -0,0 +1,21 @@
+//refactoring
+
+const myArr = ["apple", "orange"];
+
+for (let i = 0; i < myArr.length; i++) {
+    console.log(myArr[i]);
+}
+
+
+
+
+
+const myArr = ["apple", "orange"];
+
+const arrLength = myArr.length
+
+for (let i = 0; i < arrLength; i++) {
+    console.log(myArr[i]);
+}
+
+
---
+++ src/modbooks/routes.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const bookRouter = Router();
+
+const Book = require("./model");
+
+const { getAllBooks, addBook, updateBook, deleteBook } = require("./controllers");
+
+bookRouter.get("/books/getallbooks", getAllBooks); 
+
+bookRouter.post("/books/addbook", addBook);
+
+bookRouter.put("/books/updatebook", updateBook); 
+
+bookRouter.delete("/books/deleteBook", deleteBook); 
+
+module.exports = bookRouter;
+
