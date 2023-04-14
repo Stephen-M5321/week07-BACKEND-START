18a421d72a15b8c34568ba9040801838b88cf194
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-04-15 00:08:39 +0100

    delete all updated

--- src/modbooks/controllers.js
+++ src/modbooks/controllers.js
@@ -1,79 +1,106 @@
 const Book = require("./model");
 
 const getAllBooks = async (req, res) => {
-    try {
-        const books = await Book.find({});
+  try {
+    const books = await Book.find({});
 
-        const successResponse = {
-            message: "success",
-            books: books,
-        };
-    
-        res.status(200).json(successResponse)   
-      } catch (error) {
-        console.log(error);  
-        }
+    const successResponse = {
+      message: "success",
+      books: books,
     };
 
-    const addBook = async (req, res) => { 
-        try{       
-            const newBook =  await Book.create({
-              author: req.body.author,
-              genre: req.body.genre,
-              title: req.body.title,
-            });
-    
-      
-            const successResponse = {
-              messsage: "success",
-              newBook: newBook,
-            };
-      
-            res.status(201).json(successResponse)
-          } catch(error) {
-            console.log(error);
-          }
-    };
-
-
-    const updateBook = async (req, res) => {
-      try {
-        const updateBook = await Book.updateOne ({ 
-          title: req.body.title,
-      },{
-        author: req.body.author
-      });
-    
-      const successSendResponse = {
-          message: "Updated successfully",
-          book: updateBook
-        }  
-        res.status(201).json(successSendResponse)
-      } catch (error) {
-        console.log(error)
-      }
+    res.status(200).json(successResponse)
+  } catch (error) {
+    console.log(error);
   }
+};
+
+const addBook = async (req, res) => {
+  try {
+    const newBook = await Book.create({
+      author: req.body.author,
+      genre: req.body.genre,
+      title: req.body.title,
+    });
+
+
+    const successResponse = {
+      messsage: "success",
+      newBook: newBook,
+    };
+
+    res.status(201).json(successResponse)
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+const updateBook = async (req, res) => {
+  try {
+    const updateBook = await Book.updateOne({
+      title: req.body.title,
+    }, {
+      author: req.body.author
+    });
+
+    const successSendResponse = {
+      message: "Updated successfully",
+      book: updateBook
+    }
+    res.status(201).json(successSendResponse)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// const deleteBook = async (req, res) => {
+//   try {
+//     const bookList = await Book.deleteOne({
+//       title: req.body.title,
+//     })
+//     console.log(bookList)
+
+//     const successSendResponse = {
+//       message: "Deleted successfully",
+//       book: bookList
+//     }
+//     res.status(201).json(successSendResponse)
+//   } catch (error) {
+//     console.log(error);
+//     response.status(200).json(successSendResponse)
+//   }
+// }
 
 const deleteBook = async (req, res) => {
-try {
-  const bookList = await Book.deleteOne ({ 
-    title: req.body.title,
-})
-console.log(bookList)
+  try {
+    let bookList;
+    if (req.body.title) {
+      bookList = await Book.deleteOne({ title: req.body.title });
+    } else {
+      bookList = await Book.deleteMany({});
+    }
+    console.log(bookList);
 
-const successSendResponse = {
-    message: "Deleted successfully",
-    book: bookList
-}
-res.status(201).json(successSendResponse)
-} catch (error) {
-  console.log(error);
-  response.status(200).json(successSendResponse)
-}}
-
-    module.exports = {
-        getAllBooks,
-        addBook,
-        updateBook,
-        deleteBook,
+    const successSendResponse = {
+      message: "Deleted successfully",
+      book: bookList,
     };
+    res.status(201).json(successSendResponse);
+  } catch (error) {
+    console.log(error);
+    const errorSendResponse = {
+      message: "Error deleting book(s)",
+      error: error.message,
+    };
+    response.status(200).json(errorSendResponse);
+  }
+};
+
+
+module.exports = {
+  getAllBooks,
+  addBook,
+  updateBook,
+  deleteBook,
+};
